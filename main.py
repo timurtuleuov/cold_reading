015251e252325ff1f65cb69c9217bf61885b39c7
Timur Tuleuov <101930202+timurtuleuov@users.noreply.github.com>
2022-05-01 19:24:06 +0600

    create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from deepface import DeepFace
+import json
+
+def face_analyze(): #функция для анализа лиц
+    try: 
+        result_dict = DeepFace.analyze(img_path='/content/1.jpg', actions=['age','emotion','gender', 'race'], enforce_detection=False) #анализируем картинку по признакам возраст, эмоции и т.д чел ты же умеешь читать на инглише камон
+
+        with open('face_analyze.json', 'w') as file: # полученные данные записываем в жисон
+            json.dump(result_dict, file, indent=4, ensure_ascii=False)
+        
+        return result_dict
+
+    except Exception as _ex:
+        return _ex
+
+def main():
+    print(face_analyze())
+
+if __name__ == '__main__':
+    main()
