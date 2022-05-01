dbf97695874f7933f9ecf5bb9d067a0557bf3c39
Timur Tuleuov <101930202+timurtuleuov@users.noreply.github.com>
2022-05-01 19:32:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 def face_analyze(): #функция для анализа лиц
     try: 
-        result_dict = DeepFace.analyze(img_path='/content/1.jpg', actions=['age','emotion','gender', 'race'], enforce_detection=False) #анализируем картинку по признакам возраст, эмоции и т.д чел ты же умеешь читать на инглише камон
+        result_dict = DeepFace.analyze(img_path='1.jpg', actions=['age','emotion','gender', 'race'], enforce_detection=False) #анализируем картинку по признакам возраст, эмоции и т.д чел ты же умеешь читать на инглише камон
 
         with open('face_analyze.json', 'w') as file: # полученные данные записываем в жисон
             json.dump(result_dict, file, indent=4, ensure_ascii=False)
